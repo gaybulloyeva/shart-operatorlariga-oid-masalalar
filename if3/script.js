75efddc8cf3c01a1f8a98da84adc09403b4fce0d
Rayhona <gaybullayevarayhona718@gmail.com>
2024-08-25 16:56:28 +0500

    Add files via upload

---
+++ if3/script.js
@@ -0,0 +1,17 @@
+function solve() {
+    let a = prompt("butun sonni kiriting")
+    let sum = +a + 1
+    let negative = a - 2
+
+    if (a > 0) {
+        document.getElementById("demo").innerHTML = "Natija: " + sum
+    }
+
+    else if (a == 0) {
+        document.getElementById("demo").innerHTML = "Natija: " + 10
+    }
+
+    else {
+        document.getElementById("demo").innerHTML = "Natija: " + negative
+    }
+}
